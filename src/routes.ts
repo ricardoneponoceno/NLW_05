c70d3882c01560a8fdd7dc6b2e8c9c7fc21ce3f7
Ricardo Neponoceno <ricardo.neponoceno@gmail.com>
2021-04-22 02:49:19 -0300

    Aula 3 - Separação da regra de negócio do settings, criação da estrutura de user e messages (criação do relacionamento Many To one)

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import { MessagesController } from "./controllers/MessagesController";
 import { SettingsController } from "./controllers/SettingsController";
-
+import { UsersController } from "./controllers/UsersController";
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
+const messagesController = new MessagesController();
+
+routes.post("/settings", settingsController.create);
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messagesController.create);
+routes.get("/messages/:id", messagesController.showByUser);
+
+export { routes };
 
 /**
  * Tipos de parametros
@@ -12,7 +23,3 @@ const settingsController = new SettingsController();
  * Query Params => Filtros e buscas (http://localhost:3333/settings/1?search=)
  * Body Params => {}
  */
-
-routes.post("/settings", settingsController.create);
-
-export { routes };
